497a3aef2afdb54f418d34e2a04068e1058f1352
徐辰 <windoze@0d0a.com>
2025-11-29 22:43:47 +0800

    UI cleanup

--- clipper/vite.config.ts
+++ clipper/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -27,6 +28,12 @@ export default defineConfig(async () => ({
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
+    },
+    fs: {
+      // Allow serving files from the parent packages directory
+      allow: [
+        path.resolve(__dirname, ".."),
+      ],
     },
   },
 }));
